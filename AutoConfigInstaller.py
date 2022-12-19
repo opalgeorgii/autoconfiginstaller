f22f5c0030a8ec537b580a57794e1476fd1c3218
George Vayner <52776853+georgeVayner@users.noreply.github.com>
2022-12-19 19:16:33 +0200

    Add files via upload

--- AutoConfigInstaller.py
+++ AutoConfigInstaller.py
@@ -30,6 +30,8 @@ disk = 0
 d = collections.OrderedDict(kv["libraryfolders"][str(disk)]["apps"])
 items = list(d.items())
 
+GamesLibPath = kv["libraryfolders"][str(disk)]["path"]
+
 # find the location of steam library
 for disk in range(len(disksNumbers)):
     i = 0
@@ -45,33 +47,20 @@ for disk in range(len(disksNumbers)):
     disk = total
 
 # find current folder/path
-main_src_folder = str(pathlib.Path().resolve())
-cfg_dst_folder = GamesLibPath + r"\steamapps\common\Counter-Strike Global Offensive\csgo\cfg\\"
-background_dst_folder = GamesLibPath + r"\steamapps\common\Counter-Strike Global Offensive\csgo\panorama\videos\\"
+main_src_folder = str(pathlib.Path().absolute())
+cfg_dst_folder = GamesLibPath.replace("\x5c\x5c", "\x5c") + r"\steamapps\common\Counter-Strike Global Offensive\csgo\cfg" + "\x5c"
+background_dst_folder = GamesLibPath.replace("\x5c\x5c", "\x5c") + r"\steamapps\common\Counter-Strike Global Offensive\csgo\panorama\videos" + "\x5c"
 video_dst_folder = steamPath.value + r"\userdata"
 
 # list file and directories
 res = os.listdir(video_dst_folder)
 
 # Search files with the specified extension/name in source directory
-files_to_move = ["video.txt"]
+files_to_move = ['video.txt']
 pattern_1 = "\*.cfg"
 pattern_2 = "\*.webm"
 main_files = glob.glob(main_src_folder + pattern_1)
 background_files = glob.glob(main_src_folder + pattern_2)
-
-# prints all directories in a path, copies them and iterates to get to each account and paste video.txt
-for i in range(len(res)):
-    video_updated_folder = video_dst_folder + "\x5c" + str(res[i]) + r"\730\local\cfg\\"
-    print(video_updated_folder)
-    # move file whose name starts with specified name
-    for file in files_to_move:
-        # construct full file path
-        source = main_src_folder + file
-        destination = video_updated_folder + file
-        # move file
-        shutil.copy2(file, video_updated_folder)
-        print('Copied:', file)
 
 # move the files with cfg extension
 for file in main_files:
@@ -84,3 +73,16 @@ for file in background_files:
     file_name = os.path.basename(file)
     shutil.copy2(file, background_dst_folder + file_name)
     print('Copied:', file)
+
+# prints all directories in a path, copies them and iterates to get to each account and paste video.txt
+for i in range(len(res)):
+    video_updated_folder = video_dst_folder + "\x5c" + res[i] + r"\730\local\cfg" + "\x5c"
+    print(video_updated_folder)
+    # move file whose name starts with specified name
+    for file in files_to_move:
+        # construct full file path
+        source = main_src_folder + "\x5c" + file
+        destination = video_updated_folder + file
+        # move file
+        shutil.copy2(source, destination)
+        print('Copied:', file)
